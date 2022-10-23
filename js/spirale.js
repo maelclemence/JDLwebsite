013af69f56a4b3d5b47c8daa806993df5fb141c1
Alexis <alexis.tabin@gmail.com>
2022-10-23 17:59:16 +0200

    bettershow

--- js/spirale.js
+++ js/spirale.js
@@ -113,13 +113,14 @@ function showRandomDepop(depop) {
     for (var i = 0; i < arrayLength; i++) {
         console.log(depop[i]);
         article = depop[i]
-        let annonce = `
-        <ul class="list-group">
-            <li class="list-group-item">${article.id}</li>
-            <li class="list-group-item">Prix : ${article.price.priceAmount}</li>
-            <li class="list-group-item">Date de fin : ${article.slug}</li>
-        </ul>
-        `
+    // let annonce = `
+    // <ul class="list-group">
+    //     <li class="list-group-item">${article.id}</li>
+    //     <li class="list-group-item">Prix : ${article.price.priceAmount}</li>
+    //     <li class="list-group-item">Date de fin : ${article.slug}</li>
+    // </ul>
+    // `
+        let annonce = showArticleDetails(article.id, article.price.priceAmount, article.slug, "pas d'image")
         document.getElementById('depop').innerHTML += annonce;
     }
 }
@@ -219,4 +220,34 @@ async function fetchFromDepop() {
         .then(response => response.json())
         .then(data => showRandomDepop(data.products))
         .catch(err => console.log(err));
+}
+
+function showArticleDetails(articleTitle, articlePrice, articleDescription, articleImage){
+    return `<div class="my-3 p-3 bg-body rounded shadow-sm">
+        <h6 class="border-bottom pb-2 mb-0">${articleTitle}</h6>
+        <div class="d-flex text-muted pt-3">
+        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+
+        <p class="pb-3 mb-0 small lh-sm border-bottom">
+            <strong class="d-block text-gray-dark">Price</strong>
+            ${articlePrice}
+        </p>
+        </div>
+        <div class="d-flex text-muted pt-3">
+        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
+
+        <p class="pb-3 mb-0 small lh-sm border-bottom">
+            <strong class="d-block text-gray-dark">Description</strong>
+            ${articleDescription}
+        </p>
+        </div>
+        <div class="d-flex text-muted pt-3">
+        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
+
+        <p class="pb-3 mb-0 small lh-sm border-bottom">
+            <strong class="d-block text-gray-dark">Image</strong>
+            ${articleImage}
+        </p>
+        </div>
+    </div>`
 }
