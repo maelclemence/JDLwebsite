3955be88e9390e521ac0e08237bc872ea9221f72
Alexis <alexis.tabin@gmail.com>
2022-10-23 22:18:21 +0200

    testWithBetterUI

--- js/spirale.js
+++ js/spirale.js
@@ -68,13 +68,14 @@ function addArticle(article) {
         <div class="col">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img src=${article.image} alt="Image de l'article">
 
             <div class="card-body">
               <p class="card-text">${article.title}</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary" href="${article.url}" target="_blank">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary" href="https://jeudeloie.ch" target="_blank">JDL</button>
+                  <a class="btn btn-success" href="${article.url}" target="_blank">View</a>
+                  <a class="btn btn-success" href="https://jeudeloie.ch" target="_blank">JDL</a>
                 </div>
                 <small class="text-muted">${article.platform}</small>
               </div>
