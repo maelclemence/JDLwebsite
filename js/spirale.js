6a89a56d3421a9cf751955682e2e578f9266961b
Alexis <alexis.tabin@gmail.com>
2022-10-25 22:21:01 +0200

    use_promises

--- js/spirale.js
+++ js/spirale.js
@@ -146,9 +146,9 @@ async function fetchData() {
 async function fetchFromAnibis(recherche) {
     return fetch(`https://attach-cors.herokuapp.com/https://api.anibis.ch/v4/fr/search/autosuggestions?cid=1&fcid=1&fts=banane&pr=1`, {
                                 "headers": {
-                                    // "accept": "application/json",
+                                    "accept": "application/json",
                                     "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,fr;q=0.7",
-                                    "content-type": "application/json",
+                                    //"content-type": "application/json",
                                     "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
                                     "sec-ch-ua-mobile": "?0",
                                     "sec-ch-ua-platform": "\"Linux\"",
