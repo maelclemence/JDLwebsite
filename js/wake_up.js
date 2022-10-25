ae235886e4f8eec4c0878344597e09d50922773c
Timon Voide <v.timon@outlook.com>
2022-10-25 22:46:15 +0200

    hello, c'est la reussite

--- js/wake_up.js
+++ js/wake_up.js
@@ -12,9 +12,9 @@ function size() {
   var numbers = '0123456789';
   var taille = "px";
   for (var i = 0; i < 2; i++) {
-    taille = numbers[Math.floor(Math.random() * 10)] + taille ;
+    taille = numbers[Math.floor(Math.random() * 10)] + taille;
   }
-  console.log("taille:",taille) 
+  console.log("taille:", taille)
   return taille;
 }
 
@@ -22,36 +22,36 @@ function creatBackgroundColor() {
   var numbers = '0123456789';
   var taille = "px";
   for (var i = 0; i < 2; i++) {
-    taille = numbers[Math.floor(Math.random() * 10)] + taille ;
+    taille = numbers[Math.floor(Math.random() * 10)] + taille;
   }
-  console.log("taille:",taille) 
+  console.log("taille:", taille)
   return taille;
 }
 
 async function fetchData() {
 
   setColorTitle();
-  
+
 
   const response = await fetch("https://attach-cors.herokuapp.com/https://www.boredapi.com/api/activity?type=recreational", {
-          "headers": {
-              "accept": "application/json, t§ext/plain, */*",
-              "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,fr;q=0.7",
-              "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
-              "sec-ch-ua-mobile": "?0",
-              "sec-ch-ua-platform": "\"Linux\"",
+    "headers": {
+      "accept": "application/json, t§ext/plain, */*",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,fr;q=0.7",
+      "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Linux\"",
 
-          },
-          "referrer": "https://www.boredapi.com/",
-          "referrerPolicy": "strict-origin-when-cross-origin",
-          "body": null,
-          "method": "GET",
-          "mode": "cors",
-          "credentials": "omit"
-          })
-          .then(response => response.json())
-          .then(data => showRandomActivity(data.activity))
-          .catch(err => console.log(err));
+    },
+    "referrer": "https://www.boredapi.com/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "omit"
+  })
+    .then(response => response.json())
+    .then(data => showRandomActivity(data.activity))
+    .catch(err => console.log(err));
 
 
 
@@ -60,8 +60,8 @@ async function fetchData() {
 
 
 // get window width and height
-var winWidth = window.innerWidth*0.9;
-var winHeight = window.innerHeight*0.9;
+var winWidth = window.innerWidth * 0.9;
+var winHeight = window.innerHeight * 0.9;
 
 // get random numbers for each element
 randomTop = getRandomNumber(0, winHeight);
@@ -70,42 +70,46 @@ randomLeft = getRandomNumber(0, winWidth);
 
 // function that returns a random number between a min and max
 function getRandomNumber(min, max) {
-  
-return Math.random() * (max - min) + min;
+
+  return Math.random() * (max - min) + min;
 
 }
- 
+
 
 function setColorTitle() {
-  document.getElementById('title').innerHTML=`<h1  
+  document.getElementById('title').innerHTML = `<h1  
   class="text-center" style="color:${getRandomColor()};font-size: 100px " id="title" > WAKE UP! 
   </h1>`
 }
 
 var sentences = [];
 
-function changeSentencesOpacity()
-{ sentences.forEach(function(sentence){
-let result = sentence.substring(33, 36);
-console.log(result)
-mastercard=parseFloat(result);
-let visacard= 0.9*(Math.round(mastercard*10)/10)
-let backtostring= visacard.toString()
-sentence.replace(sentence.substring(33.36),backtostring)
-console.log(visacard)
-sentence.replace(result,backtostring)
-console.log(sentence.replace(result,backtostring))
-})
+function changeSentencesOpacity() {
+  for(i=0; i < sentences.length; i++){
+    sentence = sentences[i]
+    console.log("Salut:",sentence)
+      let result = sentence.substring(33, 36);
+      console.log(result)
+      let yomec = parseFloat(result)
+      console.log("yomec : ", yomec)
+      let wut = yomec.toFixed(1);
+      console.log("wut : ", wut)
+      let mastercard = 0.92*wut;
+      console.log("mastercard : ", mastercard)
+      let backtostring = mastercard.toString()
+      sentences[i] = sentence.replace(result, backtostring)
+      console.log(sentences[i])
 
-
+  }
+  console.log(sentences)
 }
 
 
 function showRandomActivity(activity) {
-const sentence = ` <p style="color:${getRandomColor()};opacity:1.0;font-size:${size()};top:${getRandomNumber(0, winHeight)}px;left:${getRandomNumber(0, winHeight)}px">${activity}</p>`;
+  const sentence = ` <p style="color:${getRandomColor()};opacity:1.0;font-size:${size()};top:${getRandomNumber(0, winHeight)}px;left:${getRandomNumber(0, winHeight)}px">${activity}</p>`;
   // iterate on list and diminish opacity of each sentence
-  changeSentencesOpacity()
   sentences.push(sentence);
+  changeSentencesOpacity()
   document.getElementById('activity').innerHTML = sentences.join();
- 
+
 }
